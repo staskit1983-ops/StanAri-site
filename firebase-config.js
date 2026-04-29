8f8202503ab62d21d49a3a274fd553a68513b6a9
staskit1983-ops <staskit1983@gmail.com>
2026-04-29 14:08:56 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  "apiKey": "AIzaSyCSBz8I0SsicV8KOf6iRpnPqGXUC3tLP4I",
+  "authDomain": "stanari-web.firebaseapp.com",
+  "projectId": "stanari-web",
+  "storageBucket": "stanari-web.firebasestorage.app",
+  "messagingSenderId": "205730592367",
+  "appId": "1:205730592367:web:dfbcadcf67816a9901ff9a",
+  "measurementId": "G-2YE7WDPVYG"
+};
+export const ADMIN_EMAIL = "stanari.art@gmail.com";

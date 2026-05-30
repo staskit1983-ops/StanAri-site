509abd6d745f14723125f84529453d6b67ffc87f
staskit1983-ops <staskit1983@gmail.com>
2026-05-31 00:23:29 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'stanari-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/reader.html',
+  '/icon-192.png',
+  '/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
